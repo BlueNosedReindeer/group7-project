68b906b3e39a1d74842c6e860a5ef734ebde5ee7
giselmn <giselmejia96@gmail.com>
2024-03-07 00:57:45 -0500

    adding a new book feature
    first mini feature(task) of book details feature

---
+++ app/Http/Controllers/BookDetailController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookDetail;
+use Illuminate\Http\Request;
+
+class BookDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BookDetail $bookDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BookDetail $bookDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BookDetail $bookDetail)
+    {
+        //
+    }
+}
